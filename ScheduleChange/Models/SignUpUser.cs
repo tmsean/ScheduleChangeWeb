b2ca8d0ece84b940d96c270edd4bfbe835fad6bc
Sean <minhson293@gmail.com>
2021-06-02 11:29:25 +0700

    Create model for User sign up

---
+++ ScheduleChange/Models/SignUpUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScheduleChange.Models
+{
+    public class SignUpUser
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a strong password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
